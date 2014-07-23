3b6ac5c0040c2140442dc3da988e41782f821434
Jean Deruelle <jean.deruelle@telestax.com>
2014-07-23 16:45:17 +0200

    MSS-112 #Resolve-Issue Fixed #comment Fix + non regression tests

--- sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/proxy/ProxyImpl.java
+++ sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/proxy/ProxyImpl.java
@@ -1,23 +1,22 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * This file incorporates work covered by the following copyright contributed under the GNU LGPL : Copyright 2007-2011 Red Hat.
  */
 
 package org.mobicents.servlet.sip.proxy;
@@ -187,15 +186,19 @@ public class ProxyImpl implements MobicentsProxy, Externalizable {
 	 */
 	private SipURI extractPreviousNodeFromRequest(SipServletRequestImpl request) {
 		SipURI uri = null;
-		try {
+//		try {
 			// First check for record route
-			RecordRouteHeader rrh = (RecordRouteHeader) request.getMessage().getHeader(RecordRouteHeader.NAME);
-			if(rrh != null) {
-				javax.sip.address.SipURI sipUri = (javax.sip.address.SipURI) rrh.getAddress().getURI();
-				uri = new SipURIImpl(sipUri, ModifiableRule.NotModifiable);
-			} else { 
-				// If no record route is found then use the last via (the originating endpoint)
-				ListIterator<ViaHeader> viaHeaders = request.getMessage().getHeaders(ViaHeader.NAME);
+//			RecordRouteHeader rrh = (RecordRouteHeader) request.getMessage().getHeader(RecordRouteHeader.NAME);
+//			if(rrh != null) {
+//				javax.sip.address.SipURI sipUri = (javax.sip.address.SipURI) rrh.getAddress().getURI();
+//				uri = new SipURIImpl(sipUri, ModifiableRule.NotModifiable);
+//			} else { 
+				// https://code.google.com/p/sipservlets/issues/detail?id=275
+				// we should use the contact header from the originating endpoint
+				ContactHeader contact = (ContactHeader) request.getMessage().getHeader(ContactHeader.NAME);
+				javax.sip.address.SipURI contactUri = ((javax.sip.address.SipURI)contact.getAddress().getURI());
+				uri = new SipURIImpl(contactUri, ModifiableRule.NotModifiable);
+				/*ListIterator<ViaHeader> viaHeaders = request.getMessage().getHeaders(ViaHeader.NAME);
 				ViaHeader lastVia = null;
 				while(viaHeaders.hasNext()) {
 					lastVia = viaHeaders.next();
@@ -206,12 +209,12 @@ public class ProxyImpl implements MobicentsProxy, Externalizable {
 					uri.setTransportParam(lastVia.getTransport());
 				} else {
 					uri.setTransportParam("udp");
-				}
-			}
-		} catch (Exception e) {
-			// We shouldn't completely fail in this case because it is rare to visit this code
-			logger.error("Failed parsing previous address ", e);
-		}
+				}*/
+//			}
+//		} catch (Exception e) {
+//			// We shouldn't completely fail in this case because it is rare to visit this code
+//			logger.error("Failed parsing previous address ", e);
+//		}
 		return uri;
 
 	}
