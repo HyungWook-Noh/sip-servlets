041b00370bbd005a91d1632e078ed0da20b6b0a6
Jean Deruelle <jean.deruelle@telestax.com>
2014-10-13 12:00:46 +0200

    MSS-120 #Resolve-Issue Fixed #comment fix + non regression test case

--- sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/TestSipListener.java
+++ sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/TestSipListener.java
@@ -1197,6 +1197,7 @@ public class TestSipListener implements SipListener {
 			ServerTransaction serverTransactionId) {
 		try {
 			cancelReceived = true;
+			Thread.sleep(waitBeforeFinalResponse);
 			SipProvider sipProvider = (SipProvider) requestEvent.getSource();
 			Request request = requestEvent.getRequest();
 			setCancelRequest(request);
